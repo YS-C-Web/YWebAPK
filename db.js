ae708254099249b33a54e8512db3043a6289efd9
YS-C-Web <yscode03@gmail.com>
2026-08-20 08:32:19 -0400

    Add files via upload

--- db.js
+++ db.js
@@ -11,7 +11,7 @@ const APPS_DB = [
     descripcion: "Te mostraremos 4 imagenes, según su contexto debes adivinar una palabra con las letras que te daremos.",
     icono: "icons/4fotos1palabra.jpeg",
     tamaño: "0.00 MB",
-    fecha: "----/--/--",
+    fecha: ".",
     link: "apks/4fotos1palabra.apk",
     screenshots: ["screenshots/4f1p1.jpg", "screenshots/4f1p2.jpg"],
     enDesarrollo: true
@@ -26,19 +26,19 @@ const APPS_DB = [
     tamaño: "4.67 MB",
     fecha: "2026-08-09",
     link: "apks/Contador_de_Dinero_Profesional.apk",
-    screenshots: ["screenshots/CdDP1.jpg", "screenshots/CdDP2.jpg"],
+    screenshots: ["screenshots/CdDP0.gif", "screenshots/CdDP1.jpg", "screenshots/CdDP2.jpg"],
   },
   {
     id: 3,
     nombre: "Contador de Dinero Universal",
-    version: "1.0",
+    version: "1.3",
     categoria: "utilidad",
     descripcion: "Contador de dinero universal con soporte para múltiples monedas.",
     icono: "icons/IcoCdDU.png",  // Reutilizado para este, pero podría ser otro
     tamaño: "5.06 MB",
     fecha: "2026-08-09",
     link: "apks/Contador_de_Dinero_Universal.apk",
-    screenshots: ["screenshots/CdDU1.jpg", "screenshots/CdDU2.jpg"]
+    screenshots: ["screenshots/CdDU0.gif", "screenshots/CdDU1.jpg", "screenshots/CdDU2.jpg"]
   },
   {
     id: 4,
@@ -50,7 +50,7 @@ const APPS_DB = [
     tamaño: "4.71 MB",
     fecha: "2026-08-09",
     link: "apks/Conversor_de_Unidades.apk",
-    screenshots: ["screenshots/CdU1.jpg", "screenshots/CdU2.jpg"]  // cdpu = Conversor De Peso Universal
+    screenshots: ["screenshots/CdU0.gif", "screenshots/CdU1.jpg", "screenshots/CdU2.jpg"]  // cdpu = Conversor De Peso Universal
   },
   {
     id: 5,
@@ -74,7 +74,7 @@ const APPS_DB = [
     tamaño: "5.02 MB",
     fecha: "2026-08-18",
     link: "apks/IPV.apk",
-    screenshots: ["screenshots/IPV1.jpg", "screenshots/IPV2.jpg", "screenshots/IPV3.jpg", "screenshots/IPV4.jpg", "screenshots/IPV5.jpg"]
+    screenshots: ["screenshots/IPV0.gif", "screenshots/IPV1.jpg", "screenshots/IPV2.jpg", "screenshots/IPV3.jpg", "screenshots/IPV4.jpg", "screenshots/IPV5.jpg"]
   },
   {
     id: 7,
